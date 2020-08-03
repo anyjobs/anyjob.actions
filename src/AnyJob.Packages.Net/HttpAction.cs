f3bac8cc4e7df598ad2b11f51bae53aa27b5f8c3
pengbo.yang <obpt@sina.com>
2020-08-04 00:31:06 +0800

    add http action

--- src/AnyJob.Packages.Net/HttpAction.cs
+++ src/AnyJob.Packages.Net/HttpAction.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Net.Http;
 using System.Text;
+using System.Linq;
 
 namespace AnyJob.Packages.Net
 {
     public class HttpAction : IAction
     {
-
         public string Url { get; set; }
 
         public string Method { get; set; } = "GET";
@@ -17,9 +18,20 @@ namespace AnyJob.Packages.Net
         {
             using (HttpClient client = new HttpClient())
             {
-                return new HttpResponse
+                client.Timeout = TimeSpan.FromSeconds(this.TimeOut);
+                using (var request = new HttpRequestMessage(new HttpMethod(this.Method), this.Url))
                 {
-
+                    var response = client.SendAsync(request).Result;
+                    return new HttpResponse
+                    {
+                        StatusCode = response.StatusCode,
+                        ReasonPhrase = response.ReasonPhrase,
+                        Version = response.Version.ToString(),
+                        IsSuccess = response.IsSuccessStatusCode,
+                        Headers = response.Headers.ToDictionary(p => p.Key, p => p.Value.Last()),
+                        Content = response.Content.ReadAsStringAsync().Result,
+                        ContentType = response.Content.Headers.ContentType.MediaType
+                    };
                 };
             }
         }
@@ -27,9 +39,15 @@ namespace AnyJob.Packages.Net
     [Serializable]
     public class HttpResponse
     {
-        public int StatusCode { get; set; }
+        public HttpStatusCode StatusCode { get; set; }
+        public string ReasonPhrase { get; set; }
+        public Dictionary<string, string> Headers { get; set; }
+        public string ContentType { get; set; }
         public string Content { get; set; }
-
+        public bool IsSuccess { get; set; }
+        public string Version { get; set; }
 
     }
 }
+
+
--- test/AnyJob.Packages.Net.UnitTest/UnitTest1.cs
+++ test/AnyJob.Packages.Net.UnitTest/UnitTest1.cs
@@ -8,6 +8,14 @@ namespace AnyJob.Packages.Net.UnitTest
         [TestMethod]
         public void TestMethod1()
         {
+            var action = new HttpAction()
+            {
+                Url = "http://news.baidu.com/ent",
+                Method = "GET"
+            };
+
+            var response = action.Run(null);
+            Assert.IsNotNull(response);
         }
     }
 }
